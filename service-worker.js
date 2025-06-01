95111b24d33523cf70df9207e122f0be91bd600f
FranciscoRodriguezCode <FranciscoRodriguezCode@gmail.com>
2025-06-01 00:46:38 -0500

    Fixing source for files

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('pip-notes-v1').then((cache) => {
             return cache.addAll([
-                './',
-                './index.html',
-                './styles/main.css',
-                './scripts/app.js',
-                './manifest.json',
-                './icons/icon-192x192.png',
-                './icons/icon-512x512.png'
+                '/fullpip-notes/',
+                '/fullpip-notes/index.html',
+                '/fullpip-notes/styles/main.css',
+                '/fullpip-notes/scripts/app.js',
+                '/fullpip-notes/manifest.json',
+                '/fullpip-notes/icons/icon-192x192.png',
+                '/fullpip-notes/icons/icon-512x512.png'
             ]);
         })
     );
