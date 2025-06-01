30491e8d8583d635b26f4b16bf513bbaa2fab905
FranciscoRodriguezCode <FranciscoRodriguezCode@gmail.com>
2025-06-01 00:22:47 -0500

    Debugging CSS layout

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('pip-notes-v1').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles/main.css',
-                '/scripts/app.js',
-                '/manifest.json',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
+                './',
+                './index.html',
+                './styles/main.css',
+                './scripts/app.js',
+                './manifest.json',
+                './icons/icon-192x192.png',
+                './icons/icon-512x512.png'
             ]);
         })
     );
